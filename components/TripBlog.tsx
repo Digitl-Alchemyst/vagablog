a3952f181b9da0c587fdd9d58cb1ca71743e17c5
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-06-08 22:08:06 -0500

    Base Layout

---
+++ components/TripBlog.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TripBlog = (props: Props) => {
+  return (
+    <div>TripBlog</div>
+  )
+}
+
+export default TripBlog
---
+++ components/WanderingThoughts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const WanderingThoughts = (props: Props) => {
+  return (
+    <div>WanderingThoughts</div>
+  )
+}
+
+export default WanderingThoughts
